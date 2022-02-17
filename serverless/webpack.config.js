ffcb5338e6d774bf539c9dac6092e1560361fd32
Nima Eskandary <nimaesk1@gmail.com>
2022-02-16 20:32:15 -0500

    get serverless webpack to work

---
+++ serverless/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  "mode": "development",
+  "target": "node12",
+  "entry": slsw.lib.entries,
+  "output": {
+      "path": path.resolve(__dirname, 'build'),
+      "filename": "[name].js",
+      "library": {
+        "type": "commonjs"
+      }
+  },
+  "watch": false,
+  "context": path.resolve(__dirname),
+  "module": {
+      "rules": [
+          {
+              "test": /\.ts?$/,
+              "exclude": /node_modules/,
+              "use": {
+                  "loader": "ts-loader",
+                  "options": {
+                      "transpileOnly": false,
+                      "projectReferences": true,
+                      "configFile": path.resolve(__dirname, "../node/packages/handlers/src/tsconfig.json")
+                  }
+              }
+          }
+      ]
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, '../node/packages/handlers/node_modules'),
+      path.resolve(__dirname, '../node/packages/handlers/'),
+      path.resolve(__dirname, '../node/node_modules'),
+      path.resolve(__dirname, '../node')
+    ],
+    extensions: [".ts", ".js"],
+    plugins: [
+      new TsconfigPathsPlugin({
+        "configFile": path.resolve(__dirname, "../node/packages/handlers/src/tsconfig.json")
+      })
+    ]
+  },
+  plugins: []
+}
